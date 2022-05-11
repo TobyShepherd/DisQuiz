f92d874d1ed5f40da2d1a30946f3f52fa82559d7
Toby Shepherd <tobias2.shepherd@live.uwe.ac.uk>
2022-05-11 15:16:54 +0100

    Started Project
    Started

---
+++ DisQuiz/Shared/DisQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisQuizApp.swift
+//  Shared
+//
+//  Created by Tobias Shepherd on 11/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct DisQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
