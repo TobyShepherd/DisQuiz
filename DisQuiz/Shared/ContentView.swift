f78b11742e216eff2d2ffa0167370d4a1e089f87
Tobias Shepherd <tobias2.shepherd@live.uwe.ac.uk>
2022-05-13 16:23:20 +0100

    Updated Functionality

--- DisQuiz/Shared/ContentView.swift
+++ DisQuiz/Shared/ContentView.swift
@@ -9,80 +9,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var actualNumber = 1
-    @State private var name: String = ""
-    @State private var comparisonText = ""
-    @State private var correctCount = 0
-    @State private var incorrectCount = 0
-    @State private var chName = ""
     var body: some View {
-        ZStack(alignment: .top){
-            Color.cyan.ignoresSafeArea()
+        NavigationView{
             ZStack{
-                VStack{
-                Text("Who am i?")
+            Color.cyan.ignoresSafeArea()
+            VStack{
+                Image("logo 2")
+                    .offset(y: -150)
+                NavigationLink(destination: GameView(totalQuestions: 10, difficulty: "HARD"))
+            {
+                Text("Quick Play!")
                     .foregroundColor(.black)
                     .font(.largeTitle)
                     .padding(.all)
-                    VStack{
-        ForEach (characters) { character in
-            let characterNumber = character.characterID
-            if(characterNumber == actualNumber)
+            }
+            NavigationLink(destination: MenuView(menuTotalQuestions: 10, menuDifficulty: "MEDIUM", difficultyCounter: 2))
             {
-            //chName = character.name
-            CharacterImage(character: character)
+                Text("Set Up Game!")
+                    .foregroundColor(.black)
+                    .font(.largeTitle)
+                    .padding(.all)
+            }
+            }
+            .offset(y: -150)
             }
         }
-                        
-        TextField(name, text: $name)
-            .background(.white)
-            .padding(.all)
-                        
-        Button(action: {
-            if(comparisonText == name)
-            {
-                correctCount = correctCount + 1
-            }
-            else
-            {
-                incorrectCount = incorrectCount + 1
-                print(incorrectCount)
-            }
-            name = ""
-            actualNumber = Int.random(in: 1...3)
-        }, label: {
-            Text("Submit")
-                .padding(.all)
-                .background(.green)
-                .foregroundColor(.black)
-                .cornerRadius(50)
-        })
-        VStack{
-            HStack{
-                Text("Correct")
-                    .foregroundColor(.black)
-                    .font(.largeTitle)
-                    .padding(.all)
-                Text("Incorrect")
-                    .foregroundColor(.black)
-                    .font(.largeTitle)
-                    .padding(.all)
-            }
-            HStack{
-                Text("correctCount")
-                    .foregroundColor(.black)
-                    .font(.largeTitle)
-                    .padding(.all)
-                Text("incorrectCount")
-                    .foregroundColor(.black)
-                    .font(.largeTitle)
-                    .padding(.all)
-            }
-        }
-    }
-}
-}
-}
 }
 }
 
@@ -101,6 +52,236 @@ struct CharacterImage: View {
                 .scaledToFit()
                 .cornerRadius(50)
                 .padding()
+        }
+    }
+}
+
+struct MenuView: View {
+    @State var menuTotalQuestions: Int
+    @State var menuDifficulty: String
+    @State var difficultyCounter: Int
+    var body: some View {
+        ZStack{
+        Color.cyan.ignoresSafeArea()
+            VStack{
+                Image("logo 2")
+                Text("Game Settings!")
+                    .foregroundColor(.black)
+                    .font(.largeTitle)
+                    .padding(.all)
+                    .font(.system(size: 25))
+                VStack{
+                    Text("Total Questions")
+                        .foregroundColor(.black)
+                        .font(.largeTitle)
+                        .padding(.all)
+                HStack{
+                    Button(action: {
+                        if (menuTotalQuestions > 5)
+                        {
+                        menuTotalQuestions = menuTotalQuestions - 5
+                        }
+                        print(menuTotalQuestions)
+                    }, label: {
+                        Image("UIButtonLEFT")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                    })
+                    
+                    let menuQuestionsDisplay = "\(menuTotalQuestions)"
+                    
+                    Text(menuQuestionsDisplay)
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .padding(.all)
+                    
+                    Button(action: {
+                        if (menuTotalQuestions < 60)
+                        {
+                        menuTotalQuestions = menuTotalQuestions + 5
+                        }
+                        print(menuTotalQuestions)
+                    }, label: {
+                        Image("UIButtonRIGHT")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                    })
+                }
+                }
+                
+                VStack{
+                    Text("Difficulty")
+                        .foregroundColor(.black)
+                        .font(.largeTitle)
+                        .padding(.all)
+                HStack{
+                    Button(action: {
+                        if (difficultyCounter == 2)
+                        {
+                            difficultyCounter = 1
+                            menuDifficulty = "EASY"
+                        }
+                        else if (difficultyCounter == 3)
+                        {
+                            difficultyCounter = 2
+                            menuDifficulty = "MEDIUM"
+                        }
+                        print(difficultyCounter)
+                        print(menuDifficulty)
+                    }, label: {
+                        Image("UIButtonLEFT")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                    })
+                    
+                    Text(menuDifficulty)
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .padding(.all)
+                    
+                    Button(action: {
+                        if (difficultyCounter == 1)
+                        {
+                            difficultyCounter = 2
+                            menuDifficulty = "MEDIUM"
+                        }
+                        else if (difficultyCounter == 2)
+                        {
+                            difficultyCounter = 3
+                            menuDifficulty = "HARD"
+                        }
+                        print(difficultyCounter)
+                        print(menuDifficulty)
+                    }, label: {
+                        Image("UIButtonRIGHT")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                    })
+                }
+                }
+                NavigationLink(destination: GameView(totalQuestions: menuTotalQuestions, difficulty: menuDifficulty))
+                {
+                    Text("Play Game!")
+                        .padding(.all)
+                        .background(.green)
+                        .foregroundColor(.black)
+                        .cornerRadius(50)
+                }
+            }
+            .offset(y: -80)
+            }
+        }
+    }
+
+struct GameView: View {
+    @State private var actualNumber = 1
+    @State private var name: String = ""
+    @State private var comparisonText = ""
+    @State private var correctCount = 0
+    @State private var incorrectCount = 0
+    @State private var chName = ""
+    @State private var characterNumber = 0
+    var totalQuestions: Int
+    var difficulty: String
+    @State var question = 0
+    var body: some View {
+        ZStack(alignment: .top){
+            Color.cyan.ignoresSafeArea()
+            ZStack{
+                VStack{
+                    Image("logo 2")
+                Text("Who am i?")
+                    .foregroundColor(.black)
+                    .font(.largeTitle)
+                    .padding(.all)
+                    VStack{
+        ForEach (characters) { character in
+            let characterNumber = character.characterID
+            //chName = character.name
+            if(characterNumber == actualNumber)
+            {
+            CharacterImage(character: character)
+            }
+        }
+                        
+        TextField(name, text: $name)
+            .background(.white)
+            .padding(.all)
+                        
+        Button(action: {
+            if(question < totalQuestions)
+            {
+                if(name != "")
+                {
+                    question = question + 1
+                    if(name != "" && comparisonText == name)
+                    {
+                        correctCount = correctCount + 1
+                    }
+                    else
+                    {
+                        incorrectCount = incorrectCount + 1
+                    }
+                    if(difficulty == "EASY")
+                    {
+                        actualNumber = Int.random(in: 1...20)
+                    }
+                    else if (difficulty == "MEDIUM")
+                    {
+                        actualNumber = Int.random(in: 1...40)
+                    }
+                    else if (difficulty == "HARD")
+                    {
+                        actualNumber = Int.random(in: 1...60)
+                    }
+                    name = ""
+                    }
+                }
+        }, label: {
+            Text("Submit")
+                .padding(.all)
+                .background(.green)
+                .foregroundColor(.black)
+                .cornerRadius(50)
+        })
+        if (question == totalQuestions)
+        {
+            NavigationLink(destination: ResultsView(correctAnswers: correctCount, incorrectAnswers: incorrectCount))
+            {
+                Text("Game Finished! Go To Results")
+                    .foregroundColor(.black)
+                    .font(.largeTitle)
+                    .padding(.all)
+            }
+        }
+    }
+}
+}
+}
+}
+}
+
+struct ResultsView: View {
+    var correctAnswers: Int
+    var incorrectAnswers: Int
+    var body: some View {
+        ZStack{
+        Color.cyan.ignoresSafeArea()
+        VStack{
+            Image("logo 2")
+            let correctDisplay = "\(correctAnswers)"
+            let incorrectDisplay = "\(incorrectAnswers)"
+            Text("Results!")
+            Text("You answered " + correctDisplay + " correctly")
+            Text("You answered " + incorrectDisplay + " incorrectly")
+            NavigationLink(destination: ContentView())
+            {
+                Text("Play Again")
+                    .foregroundColor(.black)
+                    .font(.largeTitle)
+                    .padding(.all)
+            }
+        }
         }
     }
 }
